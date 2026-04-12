c63995cf922fdbdc68ff3e84622a724a84321805
TheKnorx <the.knorx@proton.me>
2026-04-12 22:31:43 +0200

    Integrated particles-POC from animation.*

--- src/window.c
+++ src/window.c
@@ -6,13 +6,17 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include "window.h"
+
+#include "animation.h"
+
 #define THROW_ERROR(MSG, ...) do {		\
 	fprintf(stderr, MSG "\n", ##__VA_ARGS__);  \
 	return -1;  \
 }while (0);
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+static SDL_Window *window = nullptr;
+static SDL_Renderer *renderer = nullptr;
 
 void hello_world()
 {
@@ -26,8 +30,18 @@ int init_app()
 	if (! ((window = SDL_CreateWindow("My SDL3 Window Title", 800, 600, SDL_WINDOW_FULLSCREEN))) )
 		THROW_ERROR("Cound't create Window: %s", SDL_GetError());
 
-	if (! ((renderer = SDL_CreateRenderer(window, NULL))) )
+	// Set some specific behavior settings
+	SDL_SetWindowBordered(window, false);
+	SDL_SetWindowAlwaysOnTop(window, false);
+
+	if (! ((renderer = SDL_CreateRenderer(window, nullptr))) )
 		THROW_ERROR("Couldn't create window and renderer: %s", SDL_GetError());
+
+	SDL_SetRenderVSync(renderer, 1);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 250);
+	SDL_RenderClear(renderer);
+	init_particles(renderer);
+	SDL_RenderPresent(renderer);
 	return 0;
 }
 
@@ -46,9 +60,10 @@ int run_app()
 				/* or */	(event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_Q && event.key.mod & SDL_KMOD_LCTRL)
 				) running = 0;
 		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 250);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+		//SDL_SetRenderDrawColor(renderer, 0, 0, 0, 250);
+		//SDL_RenderClear(renderer);
+		//SDL_RenderPresent(renderer);
+		SDL_Delay(16);  // ~60FPS
 	}
 	return 0;
 }
