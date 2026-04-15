4cbbbb02e66c92740043bbdd010915931e676790
TheKnorx <the.knorx@proton.me>
2026-04-15 23:00:11 +0200

    Edited window and renderer configs

--- src/window.c
+++ src/window.c
@@ -22,12 +22,16 @@ int init_app()
 {
 	if (!SDL_Init(SDL_INIT_VIDEO)) THROW_ERROR("Couldn't init SDL library: %s", SDL_GetError());
 
-	if (! ((window = SDL_CreateWindow("My SDL3 Window Title", 800, 600, SDL_WINDOW_FULLSCREEN))) )
+	// SDL_Rect rect;
+	// SDL_GetDisplayUsableBounds(1, &rect);
+	if (! ((window = SDL_CreateWindow("My SDL3 Window Title", 0, 0, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_TRANSPARENT))) )
 		THROW_ERROR("Cound't create Window: %s", SDL_GetError());
 
 	// Set some specific behavior settings
+	// SDL_MaximizeWindow(window);
 	SDL_SetWindowBordered(window, false);
 	SDL_SetWindowAlwaysOnTop(window, false);
+	// SDL_SetWindowOpacity(window, 0.9f);
 
 	if (! ((renderer = SDL_CreateRenderer(window, nullptr))) )
 		THROW_ERROR("Couldn't create window and renderer: %s", SDL_GetError());
@@ -36,6 +40,12 @@ int init_app()
 	init_particles(renderer);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
+
+	SDL_Rect rect;
+	if (!SDL_GetDisplayUsableBounds(SDL_GetPrimaryDisplay(), &rect)) {
+		printf("Error: %s\n", SDL_GetError());
+	}
+	SDL_SetWindowSize(window, rect.w, rect.h);
 	return 0;
 }
 
@@ -54,7 +64,9 @@ int run_app()
 				/* or */	(event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_Q && event.key.mod & SDL_KMOD_LCTRL)
 				) running = 0;
 		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		// SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0); // transparent clear
 		SDL_RenderClear(renderer);
 		render_particles(renderer);
 		SDL_RenderPresent(renderer);
