8c72d480d75c5fe001f4009f2aeea111d3dac9ea
TheKnorx <the.knorx@proton.me>
2026-04-15 22:59:45 +0200

    New improved particle configurations

--- src/animation.h
+++ src/animation.h
@@ -2,9 +2,9 @@
 #include "SDL3/SDL_render.h"
 
 #define PARTICLES_COUNT 100  // amount of particles to create
-#define PARTICLE_SIZE_PX_LINE 4  //  width/heigth of each particle
+#define PARTICLE_SIZE_PX_LINE 2  //  width/heigth of each particle
 #define PARTICLE_SIZE_PX_AREA (PARTICLE_SIZE_PX_LINE * PARTICLE_SIZE_PX_LINE)  // each particle is of size line*line
-#define PARTICLES_SPEED 1	// speed at which the particles will move
+#define PARTICLES_SPEED 0.6	// speed at which the particles will move
 #define PARTICLES_MOUSE_DISTANCE_PX 200  // distance (int pixels) at which particles connect with the mouse
 #define PARTICLE_RESPAWN_DELAY 30  // for description see struct particle_t
 
